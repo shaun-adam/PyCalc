7d4cac173616fff50234bec988a70a12683e73e6
Shaun Storbakken <shaun.storbakken@gmail.com>
2020-11-06 22:42:48 -0700

    layout started. added bootstrap

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 print ("test")
-
-git config --global user.email "shaun.storbakken@gmail.com"
-git config --global user.name "Shaun Storbakken"
+t1 = 5
+#git config --global user.email "shaun.storbakken@gmail.com"
+#git config --global user.name "Shaun Storbakken"
+print(t1)
